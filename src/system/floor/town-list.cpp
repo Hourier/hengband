1c4d4cc0e026d2f5200c1c48485a8f312810bbc8
Hourier <66951241+Hourier@users.noreply.github.com>
2026-05-23 16:10:50 +0900

    [Refactor] 街の名前を初期化する処理を起動時に移した

--- src/system/floor/town-list.cpp
+++ src/system/floor/town-list.cpp
@@ -6,6 +6,7 @@
 #include "system/baseitem/baseitem-list.h"
 #include "util/angband-files.h"
 #include <fmt/format.h>
+#include <range/v3/view.hpp>
 #include <set>
 #include <string>
 
@@ -56,6 +57,17 @@ TownInfo &TownList::get_town(size_t index)
     }
 
     return this->towns[index];
+}
+
+bool TownList::is_all_initialized() const
+{
+    for (const auto &town : this->towns | ranges::views::drop(1) | ranges::views::take(VALID_TOWNS - 1)) {
+        if (town.get_name().empty()) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 /*!
