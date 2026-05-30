c7634a89a06f53fc6cbdb8524c7d8716a062df69
Hourier <66951241+Hourier@users.noreply.github.com>
2026-05-30 15:05:02 +0900

    [Refactor] 一時ファイル格納クラスを作った

---
+++ src/io/temp-file.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <filesystem>
+#include <fstream>
+#include <string>
+#include <string_view>
+#include <tl/optional.hpp>
+#include <vector>
+
+class TempFile {
+public:
+    TempFile();
+    TempFile(const TempFile &) = delete;
+    TempFile(TempFile &&) = delete;
+    TempFile &operator=(const TempFile &) = delete;
+    TempFile &operator=(TempFile &&) = delete;
+    ~TempFile();
+
+    const std::filesystem::path &get_path() const;
+    tl::optional<std::string> read_line();
+    std::vector<std::string> read_all();
+    void write_line(std::string_view line);
+    void write_lines(const std::vector<std::string> &lines);
+
+private:
+    std::filesystem::path path;
+    std::ifstream ifs;
+
+    void open_for_reading();
+};
